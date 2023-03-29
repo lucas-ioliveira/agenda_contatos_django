fc8879e95336664d2466038b2d053ba60d09e366
Lucas Oliveira <lucasio2008@gmail.com>
2023-03-29 19:16:46 -0300

    Configurando área administrativa do Django

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+# Modificando exibição
+class ContatoAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'nome', 'sobrenome', 'telefone')
+    # Clicaveis para edição
+    list_display_links = ('id', 'nome')
+    # Filtros
+    list_filter = ('id', 'nome')
+    # Paginação
+    list_per_page = 10
+    # Filtros
+    search_fields = ('nome', 'sobrenome', 'telefone')
+
+
+# Registrando os modelos no admin do Django.
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -21,9 +21,15 @@ nome: STR * (obrigatório)
 '''
 
 class Categoria(models.Model):
+
     nome = models.CharField(max_length=255)
 
+    # Representando a classe (objeto)
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
+
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=255)
@@ -31,6 +37,9 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
 
 
 
