eb63e11c4c91d91ace6a1112ea8af40c5d664971
Lucas Oliveira <lucasio2008@gmail.com>
2023-03-31 14:51:20 -0300

    Tratando erros http 404

--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+# Levantando erro 404 (forma 'crua')
+from django.http import Http404
 # importando o meu model
 from .models import Contato
 
@@ -16,9 +17,24 @@ def index(request):
     )
 
 
+# Levantando erro 404 (forma 'crua')
+# def ver_contato(request, contato_id):
+#     try:
+#         # Para pegar um dado do meu bd.
+#         contato = Contato.objects.get(id=contato_id)
+#         # Retorno do dado
+#         return render(
+#             request,
+#             "contatos/ver_contato.html",
+#             {"contato": contato},  # Passo a chave e o valor (contém todos os dados).
+#         )
+#     except Contato.DoesNotExist as e:
+#         raise Http404
+
+# Levantando erro 404 (forma 'convêncional')
 def ver_contato(request, contato_id):
-    # Para pegar um dado do meu bd.
-    contato = Contato.objects.get(id=contato_id)
+    # Para pegar um dado do meu bd| tratando erros
+    contato = get_object_or_404(Contato, id=contato_id)
     # Retorno do dado
     return render(
         request,
