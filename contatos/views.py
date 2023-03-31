175e9b059635eba08c6e45f8c9a0c07a8985fae2
Lucas Oliveira <lucasio2008@gmail.com>
2023-03-31 15:45:37 -0300

    Paginação adicionada views

--- contatos/views.py
+++ contatos/views.py
@@ -6,16 +6,25 @@ from django.http import Http404
 # importando o meu model
 from .models import Contato
 
+# Paginação
+from django.core.paginator import Paginator
 
-# Função que coleta todos os meus dados do db
+
+# Função que coleta todos os meus dados do db | paginação
 def index(request):
     # Para coleta todos os dados do meu bd.
     contatos = Contato.objects.all()
+    # Define a paginação e quantidades.
+    paginator = Paginator(contatos, 3)
+    # Coletando a pagina.
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     # Retorno dos dados
     return render(
         request,
         "contatos/index.html",
-        {"contatos": contatos},  # Passo a chave e o valor (contém todos os dados).
+        # Passo a chave e o valor (contém todos os dados).
+        {"contatos": contatos},
     )
 
 
